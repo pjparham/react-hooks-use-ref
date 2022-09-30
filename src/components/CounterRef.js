3f80bbc5af1d48e5ac919f035e993a63361cb706
Parker Parham <parkerparham@gmail.com>
2022-09-30 14:22:26 -0400

    Done with assignment

--- src/components/CounterRef.js
+++ src/components/CounterRef.js
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 
 function CounterRef() {
   const count = useRef(0);
-
+ 
   function handleClick() {
     count.current = count.current + 1;
     console.log(count.current);
